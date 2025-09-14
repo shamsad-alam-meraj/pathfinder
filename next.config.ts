54b0fe16b9064dedc1dd1ffd01f881c2071c87a8
Shamsad Alam Meraj <se.fe2@ekkbaz.com>
2025-09-14 18:27:30 +0600

    nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ["via.placeholder.com", "lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com"],
   },
 };
 
