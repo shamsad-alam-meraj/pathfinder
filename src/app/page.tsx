6cda1d5899e8a823daefe38bec926b63e8a8f8c5
Shamsad Alam Meraj <se.fe2@ekkbaz.com>
2025-09-14 18:31:37 +0600

    trips page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import TripList from "./trips/page";
-
 export default function Home() {
   return (
     <div className="">
       <h1 className="text-4xl font-bold italic text-center my-12">
         Path Finder
       </h1>
-      <TripList></TripList>
     </div>
   );
 }
