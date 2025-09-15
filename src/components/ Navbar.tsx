f5243bc47d0abd2afbe9ac65b820fa7891fad25c
Shamsad Alam Meraj <se.fe2@ekkbaz.com>
2025-09-15 15:14:38 +0600

    landing page responsive

--- src/components/ Navbar.tsx
+++ src/components/ Navbar.tsx
@@ -6,16 +6,19 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="py-5 px-10 flex  items-center border-b">
+    <div className="py-5 px-10 lg:flex  items-center border-b  hidden">
       {/* image/ logo  */}
-      <div className="flex-1">
-        <Link href="/">
+      <div className="flex-1 ">
+        <Link className="flex items-center" href="/">
           <Image
-            src="/images/logo/logo_name.png"
+            src="/images/logo/logo.png"
             alt="logo"
-            width={100}
+            width={50}
             height={50}
           ></Image>
+          <h6 className="pl-3 font-bold text-2xl text-purple-800">
+            Path Finder
+          </h6>
         </Link>
       </div>
       {/* navigation buttons  */}
