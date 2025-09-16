9151f0e12151dd1603d22519a7745388b3792b6e
Shamsad Alam Meraj <se.fe2@ekkbaz.com>
2025-09-16 18:33:13 +0600

    trip list updated

--- src/lib/dummy-data.ts
+++ src/lib/dummy-data.ts
@@ -5,7 +5,7 @@ export const tripData = [
     date: "Oct 1–10, 2025",
     location: "Japan",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1579525108311-0c5730b5799d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const tripData = [
     date: "Dec 20–30, 2025",
     location: "Paris, France",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const tripData = [
     date: "Nov 5–15, 2025",
     location: "London, UK",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ export const tripData = [
     date: "Sep 10–20, 2025",
     location: "New York, USA",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1546436836-07a91091f160?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ export const tripData = [
     date: "Oct 15–25, 2025",
     location: "Sydney, Australia",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1523428096881-5bd79d043006?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ export const tripData = [
     date: "Nov 1–10, 2025",
     location: "Rome, Italy",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1096&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 7,
@@ -53,7 +53,7 @@ export const tripData = [
     date: "Dec 5–15, 2025",
     location: "Iceland",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1517928260182-5688aead3066?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 8,
@@ -61,7 +61,7 @@ export const tripData = [
     date: "Oct 20–30, 2025",
     location: "Bali, Indonesia",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1540541338287-41700207dee6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 9,
@@ -77,7 +77,7 @@ export const tripData = [
     date: "Dec 1–10, 2025",
     location: "Thailand",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://plus.unsplash.com/premium_photo-1697730197947-f19e92f0035b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 11,
@@ -85,7 +85,7 @@ export const tripData = [
     date: "Sep 20–30, 2025",
     location: "Canada",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1587381419916-78fc843a36f8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 12,
@@ -93,7 +93,7 @@ export const tripData = [
     date: "Oct 5–15, 2025",
     location: "Santorini, Greece",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1678266561093-324802646fb2?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 13,
@@ -101,7 +101,7 @@ export const tripData = [
     date: "Nov 10–20, 2025",
     location: "Morocco",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1569383746724-6f1b882b8f46?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 14,
@@ -109,7 +109,7 @@ export const tripData = [
     date: "Sep 25–Oct 5, 2025",
     location: "Alaska, USA",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1605978221138-0be1cae9204b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 15,
@@ -117,7 +117,7 @@ export const tripData = [
     date: "Oct 10–20, 2025",
     location: "Vietnam",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1531737212413-667205e1cda7?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 16,
@@ -125,7 +125,7 @@ export const tripData = [
     date: "Nov 20–30, 2025",
     location: "Egypt",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://plus.unsplash.com/premium_photo-1664303467567-17891a27998a?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 17,
@@ -133,7 +133,7 @@ export const tripData = [
     date: "Dec 10–20, 2025",
     location: "Brazil",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1742541922679-95cc82ba6c7d?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 18,
@@ -141,7 +141,7 @@ export const tripData = [
     date: "Oct 25–Nov 5, 2025",
     location: "Greece",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1498503182468-3b51cbb6cb24?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 19,
@@ -149,7 +149,7 @@ export const tripData = [
     date: "Nov 5–15, 2025",
     location: "South Africa",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1481889617387-82a8f2413b6b?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 20,
@@ -157,6 +157,6 @@ export const tripData = [
     date: "Dec 15–25, 2025",
     location: "Hawaii, USA",
     image:
-      "https://lh3.googleusercontent.com/gpms-cs-s/AB8u6HaOdlGfDvI9kDzm7zVYz9eQKT1Lnt1vaGHK3J16VF900T9SWcikvCskB_a1SJ5o2BPi-f7V0su-AUzKAEjLvKNX4bnqG-V18JYETG73PDiFhdEVnOLt_gq0yJlGWbRvhRB1MyWz=s680-w680-h510-rw",
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
